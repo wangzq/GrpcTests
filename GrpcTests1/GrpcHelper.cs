4475163a7f0042f4685c2d02301770b48bc3dd9a
wangzq <wangzq@gmail.com>
2019-08-27 09:16:06 -0700

    Enable debug log level from Grpc

--- GrpcTests1/GrpcHelper.cs
+++ GrpcTests1/GrpcHelper.cs
@@ -1,41 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Grpc.Core;
+using Grpc.Core.Logging;
 
-	using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using Grpc.Core;
-
+namespace GrpcTests
+{
     public static class GrpcHelper
-	{
-		public static Server StartServer(IList<ServerServiceDefinition> services, ServerCredentials serverCredentials, string host, int basePort, int count, out int port)
-		{
-			var options = new List<ChannelOption>
-			{
-				 new ChannelOption(ChannelOptions.MaxSendMessageLength, int.MaxValue),
-				 new ChannelOption(ChannelOptions.MaxReceiveMessageLength, int.MaxValue),
-			};
+    {
+        public static Server StartServer(IList<ServerServiceDefinition> services, ServerCredentials serverCredentials, string host, int basePort, int count, out int port)
+        {
+            UseConsoleVerboseLogger();
 
-			for (var i = 0; i < count; i++)
-			{
-				var server = new Server(options);
-				foreach (var service in services)
-				{
-					server.Services.Add(service);
-				}
+            var options = new List<ChannelOption>
+            {
+                new ChannelOption(ChannelOptions.MaxSendMessageLength, int.MaxValue),
+                new ChannelOption(ChannelOptions.MaxReceiveMessageLength, int.MaxValue),
+            };
 
-				server.Ports.Add(host, basePort + i, serverCredentials);
-				try
-				{
-					server.Start();
-					port = basePort + i;
-					return server;
-				}
-				catch (IOException ex) when (i < count - 1 &&
-				                             ex.Message.IndexOf("Failed to bind port",
-					                             StringComparison.OrdinalIgnoreCase) >= 0)
-				{
-				}
-			}
+            for (var i = 0; i < count; i++)
+            {
+                var server = new Server(options);
+                foreach (var service in services)
+                {
+                    server.Services.Add(service);
+                }
 
-			throw new InvalidOperationException($"Failed to start server at {host} using port range [{basePort}-{basePort+count})");
-		}
-	}
+                server.Ports.Add(host, basePort + i, serverCredentials);
+                try
+                {
+                    server.Start();
+                    port = basePort + i;
+                    return server;
+                }
+                catch (IOException ex) when (i < count - 1 &&
+                                             ex.Message.IndexOf("Failed to bind port",
+                                                 StringComparison.OrdinalIgnoreCase) >= 0)
+                {
+                }
+            }
+
+            throw new InvalidOperationException($"Failed to start server at {host} using port range [{basePort}-{basePort+count})");
+        }
+
+        public static void UseConsoleVerboseLogger()
+        {
+            var logger = new LogLevelFilterLogger(new ConsoleLogger(), LogLevel.Debug, false);
+            GrpcEnvironment.SetLogger(logger);
+        }
+    }
+}
